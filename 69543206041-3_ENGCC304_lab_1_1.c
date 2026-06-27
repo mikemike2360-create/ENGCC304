992c00ddcee69f176bea5f31de0bae6ecdccacb0
Mikemaster007 <mikemike2360@gmail.com>
2026-06-27 17:26:56 +0700

    Add files via upload

---
+++ 69543206041-3_ENGCC304_lab_1_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    
+    scanf("%s", name);
+    
+    printf("Hello, %s\n", name);
+    printf("Welcome to C Programming\n");
+
+    return 0;
+}
