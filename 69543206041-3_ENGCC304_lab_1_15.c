7172b84573dfe63ceef628ca8de1206677dff9dc
Mikemaster007 <mikemike2360@gmail.com>
2026-06-27 14:39:24 +0700

    Add files via upload

---
+++ 69543206041-3_ENGCC304_lab_1_15.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char in[50],po[50],out[50];
+    
+    scanf("%s %s %s", in, po, out);
+    
+    printf("Input: %s\nProcess: %s\nOutput: %s\n", in, po, out);
+   
+    return 0;
+}
